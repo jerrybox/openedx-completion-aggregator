30e6505d81fd1fe9d55a68adb2617d59658ed938
jerrybox <jerryyoucan@foxmail.com>
2020-06-30 13:58:22 +0800

    修复更改选课模式重新计算进度bug

--- completion_aggregator/tasks/handler_tasks.py
+++ completion_aggregator/tasks/handler_tasks.py
@@ -5,6 +5,7 @@ Tasks used in processing signal handlers.
 import six
 from celery import shared_task
 from celery_utils.logged_task import LoggedTask
+from opaque_keys.edx.keys import CourseKey
 
 from django.conf import settings
 
